5675da6427969bea9472941083a40632d56b7102
LicorneRose765 <210376@umons.ac.be>
2023-04-20 17:22:42 +0200

    [ADD] initial commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <signal.h>
+
+
+/******************************************************************************
+* S-INFO-111 --- Solution pour Travail 1                                      *
+* Groupe 03                                                                   * 
+* Membres:                                                                    *
+* - BERNARD Thomas                                                            *
+* - MOREAU Arnaud                                                             *
+* - MOREAU Cyril                                                              *
+******************************************************************************/
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return EXIT_SUCCESS;
+}
