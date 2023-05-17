0e013f98c77757ea2806c3f50c64f670a63ba676
Nephty <211260@umons.ac.be>
2023-05-17 17:30:52 +0200

    added shared memory methods

--- main.c
+++ main.c
@@ -22,6 +22,7 @@
 
 #define CLIENT_COUNT 5
 #define GUICHET_COUNT 4
+#define IPC_ERROR -1
 typedef enum {
     TASK1, TASK2, TASK3
 } task_t;
@@ -107,6 +108,42 @@ int guichet_behavior() {
     guichet.type_demande = TASK1; // TODO : random
 
     return EXIT_SUCCESS;
+}
+
+
+static int get_block(char *filename, int size) {
+    key_t ipc_key;
+    // Try to get a key for the file
+    ipc_key = ftok(filename, 4242);
+    // No key = error
+    if (ipc_key == IPC_ERROR) return IPC_ERROR;
+    // Use the key to create/get a block ID associated with the key
+    return shmget(ipc_key, size, 0644 | IPC_CREAT)
+}
+
+
+char *load_block(char *filename, int size) {
+    int block_id = get_block(filename, size);
+    char *result;
+    // No block ID = error
+    if (block_id == IPC_ERROR) return NULL;
+    // Map the block into the process' address space
+    // to get a pointer to the block using the block ID
+    result = shmat(block_id, NULL, 0);
+    // No result = no block
+    if (result == (char *) IPC_ERROR) return NULL;
+    return result;
+}
+
+
+int detach_block(char *block) {
+    return (shmdt(block) != IPC_ERROR);
+}
+
+int destroy_block(char *filename) {
+    int id = get_block(filename, 0);
+    if (id == IPC_ERROR) return -1;
+    return (shmctl(id, IPC_RMID, NULL) != IPC_ERROR);
 }
 
 
