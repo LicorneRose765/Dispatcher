c6472efc54f32a5017fc21b7a36ab850b7a08fac
Nephty <211260@umons.ac.be>
2023-05-18 12:59:45 +0200

    moved block methods declaration, added write_to_block method

--- main.c
+++ main.c
@@ -21,11 +21,77 @@
 * - MOREAU Cyril                                                              *
 ******************************************************************************/
 
-#define CLIENT_COUNT 5
+#define CLIENT_COUNT 1
 #define GUICHET_COUNT 4
 #define IPC_ERROR -1
-#define BLOCK_SIZE 4096
+#define WRITING_SUCCESS 1
+#define WRITING_ERROR -1
+#define BLOCK_SIZE 12
 #define NUMBER_OF_REQUESTS 1
+
+
+static int get_block(char *filename, int size) {
+    key_t ipc_key;
+    // Try to get a key for the file
+    ipc_key = ftok(filename, 4242);
+    // No key = error
+    if (ipc_key == IPC_ERROR) return IPC_ERROR;
+    // Use the key to create/get a block ID associated with the key
+    return shmget(ipc_key, size, 0644 | IPC_CREAT);
+}
+
+
+char *load_block(char *filename, int size) {
+    int block_id = get_block(filename, size);
+    char *result;
+    // No block ID = error
+    if (block_id == IPC_ERROR) return NULL;
+    // Map the block into the process' address space
+    // to get a pointer to the block using the block ID
+    result = shmat(block_id, NULL, 0);
+    // No result = no block
+    if (result == (char *) IPC_ERROR) return NULL;
+    return result;
+}
+
+
+int detach_block(char *block) {
+    return (shmdt(block) != IPC_ERROR);
+}
+
+int destroy_block(char *filename) {
+    int id = get_block(filename, 0);
+    if (id == IPC_ERROR) return -1;
+    return (shmctl(id, IPC_RMID, NULL) != IPC_ERROR);
+}
+
+
+int write_to_block(char *str, char *block, char mode) {
+    switch (mode) {
+        // Write mode (overwrite everything)
+        case 'w':
+            // Prevent buffer overflow
+            if (strlen(str) > BLOCK_SIZE) {
+                perror("Not enough space in block to write string.");
+                return WRITING_ERROR;
+            } else strncpy(block, str, BLOCK_SIZE);
+            return WRITING_SUCCESS;
+            // Append mode (add to end of existing data)
+        case 'a':
+            // Prevent buffer overflow
+            int remaining_space = BLOCK_SIZE - strlen(block);
+            if (strlen(str) > remaining_space) {
+                perror("Not enough space remaining in block to write string.");
+                return WRITING_ERROR;
+            } else strncpy(block, str, BLOCK_SIZE);
+            return WRITING_SUCCESS;
+        default:
+            perror("Invalid mode.\n");
+            return WRITING_ERROR;
+    }
+}
+
+
 typedef enum {
     TASK1, TASK2, TASK3
 } task_t;
@@ -41,7 +107,7 @@ typedef struct {
     // TODO : ajouter le temps min et max
     time_t temps_min;
     time_t temps_max;
-    task_t* demandes;
+    task_t *demandes;
 
 } Client;
 
@@ -95,7 +161,8 @@ int client_behavior(char *block) {
         // sleep(client.temps_min);
         // TODO : envoyer une demande
         printf("Writing some data\n");
-        strncpy(block, "I'm some data", BLOCK_SIZE);
+        write_to_block("I'm some data", block, 'a');
+        // strncpy(block, "I'm some data", BLOCK_SIZE);
         // snprintf(block, BLOCK_SIZE, "I'm some data");
         sent++;
         // sleep(client.temps_max);
@@ -118,42 +185,6 @@ int guichet_behavior() {
     guichet.type_demande = TASK1; // TODO : random
 
     return EXIT_SUCCESS;
-}
-
-
-static int get_block(char *filename, int size) {
-    key_t ipc_key;
-    // Try to get a key for the file
-    ipc_key = ftok(filename, 4242);
-    // No key = error
-    if (ipc_key == IPC_ERROR) return IPC_ERROR;
-    // Use the key to create/get a block ID associated with the key
-    return shmget(ipc_key, size, 0644 | IPC_CREAT);
-}
-
-
-char *load_block(char *filename, int size) {
-    int block_id = get_block(filename, size);
-    char *result;
-    // No block ID = error
-    if (block_id == IPC_ERROR) return NULL;
-    // Map the block into the process' address space
-    // to get a pointer to the block using the block ID
-    result = shmat(block_id, NULL, 0);
-    // No result = no block
-    if (result == (char *) IPC_ERROR) return NULL;
-    return result;
-}
-
-
-int detach_block(char *block) {
-    return (shmdt(block) != IPC_ERROR);
-}
-
-int destroy_block(char *filename) {
-    int id = get_block(filename, 0);
-    if (id == IPC_ERROR) return -1;
-    return (shmctl(id, IPC_RMID, NULL) != IPC_ERROR);
 }
 
 
